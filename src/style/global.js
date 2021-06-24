8bb71450941d35807a06d220de568a561f7d7a23
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-06-24 02:51:59 -0300

    code: add createReactAndProjectBase

---
+++ src/style/global.js
@@ -0,0 +1,46 @@
+import {createGlobalStyle} from 'styled-components';
+
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        background-image: linear-gradient(to right, rgb(230, 218, 218),rgb(239, 187, 230), rgba(153, 218, 255, 0.822),rgba(255, 254, 168, 0.822)80%);
+    }
+
+    html,body, #root{
+        height: 100%
+        
+    }
+
+    body, input, button, select{
+        font: 16px 'Roboto';
+    }
+
+    body::-webkit-scrollbar {
+        width: 0.30rem;
+    }
+      
+    body::-webkit-scrollbar-track {
+        background: #1e1e24;
+    }
+      
+    body::-webkit-scrollbar-thumb {
+        background: #5a5adf;
+    }
+
+    button{
+        curso: pointer;
+    }
+
+    #root{
+        max-width: 900px;
+        margin: 0 auto;
+        padding: 40px  20px;
+    }
+
+`;
